986edf0e69988c7e12f4fbec6b41babe99419ea3
qingsongyoudao <dawuhan08@163.com>
2020-06-28 17:03:16 +0800

    update

--- cloudfunctions-tcb/news/index.js
+++ cloudfunctions-tcb/news/index.js
@@ -14,8 +14,50 @@ exports.main = async (event, context) => {
 	let res = {}
 
 	switch (action) {
-		case 'category':
+		case 'list':
 			res = await qsNews.newsList(params);
+			break;
+		case 'detail':
+			res = await qsNews.newsDetail(params);
+			break;
+		case 'delete':
+			res = await qsNews.newsDelete(params);
+			break;
+		case 'add':
+			res = await qsNews.newsAdd(params);
+			break;
+		case 'update':
+			res = await qsNews.newsUpdate(params);
+			break;
+		case 'category-list':
+			res = await qsNews.newsCategoryList(params);
+			break;
+		case 'category-detail':
+			res = await qsNews.newsCategoryDetail(params);
+			break;
+		case 'category-delete':
+			res = await qsNews.newsCategoryDelete(params);
+			break;
+		case 'category-add':
+			res = await qsNews.newsCategoryAdd(params);
+			break;
+		case 'category-update':
+			res = await qsNews.newsCategoryUpdate(params);
+			break;
+		case 'tag-list':
+			res = await qsNews.newsTagList(params);
+			break;
+		case 'tag-detail':
+			res = await qsNews.newsTagDetail(params);
+			break;
+		case 'tag-delete':
+			res = await qsNews.newsTagDelete(params);
+			break;
+		case 'tag-add':
+			res = await qsNews.newsTagAdd(params);
+			break;
+		case 'tag-update':
+			res = await qsNews.newsTagUpdate(params);
 			break;
 		default:
 			res = {
